f093e0bb7b1d218c3b77ce59fc9d9b2141411cca
dinizdv <dinizdev3@gmail.com>
2024-11-20 17:14:27 -0300

    Change UsuarioReturnDTO

--- src/main/java/com/api_controle_acesso/DTOs/UsuarioDTO/UsuarioReturnDTO.java
+++ src/main/java/com/api_controle_acesso/DTOs/UsuarioDTO/UsuarioReturnDTO.java
@@ -1,10 +1,11 @@
 package com.api_controle_acesso.DTOs.UsuarioDTO;
 import com.api_controle_acesso.DTOs.SetorDTO.SetorReturnDTO;
 import com.api_controle_acesso.models.Usuario;
+import com.api_controle_acesso.models.enums.Role;
 
-public record UsuarioReturnDTO(Long id, String nome, SetorReturnDTO setor, String email) {
+public record UsuarioReturnDTO(Long id, String nome, SetorReturnDTO setor, String email, Role role) {
     
     public UsuarioReturnDTO(Usuario usuario) {
-        this(usuario.getId(), usuario.getNome(), new SetorReturnDTO(usuario.getSetor()), usuario.getEmail());
+        this(usuario.getId(), usuario.getNome(), new SetorReturnDTO(usuario.getSetor()), usuario.getEmail(), usuario.getRole());
     }
 }
